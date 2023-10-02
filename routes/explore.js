ac427a574e8527816e7d4e68211784a1552d3b66
amisha26 <amisha8299@gmail.com>
2023-10-01 23:04:07 -0400

    moved topic mapping to project data

--- routes/explore.js
+++ routes/explore.js
@@ -2,13 +2,6 @@ const router = require("express").Router();
 const Questions = require("../models/Questions");
 const UserQuestions = require("../models/UserQuestions");
 
-topicMapping = {"twoPointers": "Two Pointers",
-                    "strings": "Strings", "arrays": "Arrays","stack":"Stack",
-                    "binarySearch":"Binary Search","linkedlist":"Linked List",
-                    "tree-1":"Tree - 1","tree-2":"Tree - 2","dp-1":"Dynamic Programming - 1",
-                    "heap":"Heap - Priority Queue","dp-2":"Dynamic Programming - 2",
-                    "slidingWindow":"Sliding Window"}
-
 
 // formattedData = {"title": title_name, "urlTitle": urltitle, "total": total, "solved": solved}
 //GET TOPICS
