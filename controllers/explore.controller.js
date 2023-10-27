d4b652aa72092a75a941c6bf0c5ad513aebda2da
amisha26 <amisha8299@gmail.com>
2023-10-27 19:23:58 -0400

    refactored explore code

---
+++ controllers/explore.controller.js
@@ -0,0 +1,165 @@
+const Questions = require("../models/Questions");
+const UserQuestions = require("../models/UserQuestions");
+const topicMapping = require("../projectData/topicData");
+
+
+// ======================= EXPLORE TOPICS =======================
+const getExploreTopics = async (req, res) => {
+    try {
+        // request
+        const { id } = req.query;
+        const questions = await Questions.find();
+        const userQuestions = await UserQuestions.find({ userId: id });
+
+        countQuestionsPerTopic = {};
+        questions.forEach(({ topicName }) => {
+            countQuestionsPerTopic = { ...countQuestionsPerTopic, [topicName]: countQuestionsPerTopic[topicName] === undefined ? 1 : countQuestionsPerTopic[topicName] + 1 };
+        });
+
+        solvedTopics = {};
+        userQuestions.forEach(({ topicName }) => {
+            solvedTopics = { ...solvedTopics, [topicName]: solvedTopics[topicName] === undefined ? 1 : solvedTopics[topicName] + 1 }
+        });
+
+        finalData = Object.keys(countQuestionsPerTopic).map((item) => {
+            urlTitle = item;
+            total = countQuestionsPerTopic[item]
+            if (urlTitle in topicMapping) {
+                title = topicMapping[urlTitle];
+            }
+            if (urlTitle in solvedTopics) {
+                solved = solvedTopics[urlTitle];
+            }
+            else {
+                solved = 0;
+            }
+            return { "title": title, "urlTitle": urlTitle, "total": total, "solved": solved };
+        });
+
+        finalData.sort((a, b) => a.title.localeCompare(b.title));
+        // response data
+        const response = {
+            data: finalData,
+            onGoingTopic: {
+                data: "arrays",
+                onGoingTopic: true,
+            },
+        };
+
+        return res.status(200).json({ data: response, error: false });
+
+
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({ data: "Something Went Wrong!", error: true });
+    }
+};
+
+
+// ======================== SELECTED TOPIC ==========================
+const getSelectedTopics = async (req, res) => {
+    try {
+        // request
+        const { id, topic } = req.query;
+        
+        const questions = await Questions.find({ topicName: topic });
+        const userQuestions = await UserQuestions.find({ userId: id, topicName: topic });
+
+        let easyArr = [];
+        let mediumArr = [];
+        let hardArr = [];
+
+
+        questions.forEach(({ _id, questionName, platform, level, questionUrl }) => {
+            const qId = _id;
+            const qName = questionName;
+            const qPlatform = platform;
+            const qLevel = level;
+            const qUrl = questionUrl;
+
+            const findUserCompletedQues = userQuestions.find(item => item.questionId === qId);
+            let completed = findUserCompletedQues !== undefined; 
+
+            const formattedData = {
+                completed,
+                id: qId,
+                name: qName,
+                platform: qPlatform,
+                level: qLevel,
+                url: qUrl
+            };
+            if (qLevel === "easy") {
+                easyArr.push(formattedData)
+            }
+            else if (qLevel === "medium") {
+                mediumArr.push(formattedData)
+            }
+            else if (qLevel === "hard") {
+                hardArr.push(formattedData)
+            }
+
+        });
+        const selectedTopicData = [{ "body": easyArr, "cardTitle": "Easy", "cardType": "easy" },
+        { "body": mediumArr, "cardTitle": "Medium", "cardType": "medium" },
+        { "body": hardArr, "cardTitle": "Hard", "cardType": "hard" }];
+
+        // response
+        return res.status(200).json({ data: selectedTopicData, error: false });
+
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({ data: "Something Went Wrong!", error: true });
+    }
+};
+
+
+
+// Add Questions (admin only)
+const addQuestion = async (req, res) => {
+    try {
+        const {questionUrl} = req.body;
+        const questionUrlExists = await Questions.findOne({ questionUrl: questionUrl });
+        if (!questionUrlExists) {
+            const newQuestion = new Questions(req.body);
+            await newQuestion.save();
+            return res.status(200).json("Question successfully added");
+        } else {
+            return res.status(409).json("Question already exists");
+        }
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json(err);
+    }
+};
+
+
+
+// ======================= MARK QUESTION =======================
+const markQuestion = async (req, res) => {
+    try {
+        // request
+        const { user_id, question_id, topic } = req.body;
+
+        const userQuestionExists = await UserQuestions.findOne({ userId: user_id, questionId: question_id, topicName: topic });
+        // mark question
+        if (!userQuestionExists) {
+            const currentDate = new Date();
+            const markedQuestion = new UserQuestions({ userId: user_id, questionId: question_id, topicName: topic, date: currentDate });
+            await markedQuestion.save();
+            // response
+            return res.status(200).json({ data: "Marked", error: false });
+        }
+        // unmark question
+        await UserQuestions.deleteOne(userQuestionExists);
+
+        // response
+        return res.status(200).json({ data: "Question unmarked successfully", error: true });
+
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({ data: "Something went wrong", error: true });
+    }
+};
+
+
+module.exports = { getExploreTopics, getSelectedTopics, addQuestion, markQuestion };
--- routes/explore.js
+++ routes/explore.js
@@ -1,150 +1,21 @@
 const router = require("express").Router();
-const { selectedTopicData } = require("../projectData/mockApiData/dummy");
-const Questions = require("../models/Questions");
-const UserQuestions = require("../models/UserQuestions");
-const topicMapping = require("../projectData/topicData");
+const {getExploreTopics, getSelectedTopics, addQuestion, markQuestion} = require("../controllers/explore.controller");
 
 
-// formattedData = {"title": title_name, "urlTitle": urltitle, "total": total, "solved": solved}
 //GET TOPICS
-router.get("/topics", async (req, res) => {
-    try {
-        const { id: userId } = req.query;
-        const question = await Questions.find();
-        countTitle = {};
-        question.forEach((item) => {
-            val = item.topicName;
-            countTitle = { ...countTitle, [val]: countTitle[val] === undefined ? 1 : countTitle[val] + 1 };
-        });
-        const userQuestion = await UserQuestions.find();
-        solvedTopics = {};
-        userQuestion.forEach((item) => {
-            val = item.topicName;
-            id = item.userId;
-            if (id === userId) {
-                solvedTopics = { ...solvedTopics, [val]: solvedTopics[val] === undefined ? 1 : solvedTopics[val] + 1 }
-            }
-        });
-        finalData = Object.keys(countTitle).map((item) => {
-            urlTitle = item;
-            total = countTitle[item]
-            if (urlTitle in topicMapping) {
-                title = topicMapping[urlTitle];
-            }
-            if (urlTitle in solvedTopics) {
-                solved = solvedTopics[urlTitle];
-            }
-            else {
-                solved = 0;
-            }
-            return { "title": title, "urlTitle": urlTitle, "total": total, "solved": solved };
-        });
-        // response data
-        const response = {
-            data: finalData,
-            onGoingTopic: {
-                data: "arrays",
-                onGoingTopic: true,
-            },
-        };
-
-        return res.status(200).json({ data: response, error: false });
-
-
-    } catch (err) {
-        console.log(err);
-        return res.status(500).json({data: "Something Went Wrong!", error: true});
-    }
-});
-
+router.get("/topics", getExploreTopics);
+        
 
 // GET SELECTED TOPICS
-router.get("/selected_topic", async (req, res) => {
-    try {
-        const { id, topic } = req.query;
-        const question = await Questions.find();
-        easyArr = [];
-        mediumArr = [];
-        hardArr = [];
-        const userQues = await UserQuestions.find();
-        question.forEach((item) => {
-            if (item.topicName === topic) {
-                qId = item.id
-                qName = item.questionName
-                platform = item.platform
-                level = item.level
-                qUrl = item.questionUrl
-                const findUserCompletedQues = userQues.filter((i) => {
-                    return i.userId === id && i.questionId === qId;
-                });
-                if (findUserCompletedQues.length > 0) {
-                    completed = true;
-                } else {
-                    completed = false;
-                }
-                formattedData = { "completed": completed, "id": qId, "name": qName, "platform": platform, "level": level, "url": qUrl}
-                if (level === "easy") {
-                    easyArr.push(formattedData)
-                }
-                else if (level === "medium") {
-                    mediumArr.push(formattedData)
-                }
-                else if (level === "hard") {
-                    hardArr.push(formattedData)
-                }
-            }
-        });
-        const selectedTopicData = [{ "body": easyArr, "cardTitle": "Easy", "cardType": "easy" },
-        { "body": mediumArr, "cardTitle": "Medium", "cardType": "medium" },
-        { "body": hardArr, "cardTitle": "Hard", "cardType": "hard" }];
-        return res.status(200).json({ data: selectedTopicData, error: false });
-
-    } catch (err) {
-        console.log(err);
-        return res.status(500).json({data: "Something Went Wrong!", error: true});
-    }
-});
-
+router.get("/selected_topic", getSelectedTopics);
 
 
 // Add Questions (admin only)
-router.post("/add-questions", async (req, res) => {
-    try {
-        const questionUrlExists = await Questions.findOne({ questionUrl: req.body.questionUrl });
-        if (!questionUrlExists) {
-            const newQuestion = new Questions(req.body);
-            await newQuestion.save();
-            return res.status(200).json("Question successfully added");
-        } else {
-            return res.status(409).json("Question already exists");
-        }
-    } catch (err) {
-        console.log(err);
-        return res.status(500).json(err);
-    }
-});
+router.post("/add-questions", addQuestion);
 
-// Mark question
-router.post("/markQuestion", async (req, res) => {
-    try {
-        const { user_id, question_id, topic } = req.body;
-        const userQuestionExists = await UserQuestions.findOne({ userId: user_id, questionId: question_id, topicName: topic });
-        // MARK QUESTION
-        if (!userQuestionExists) {
-            const currentDate = new Date();
-            const markedQuestion = new UserQuestions({ userId: user_id, questionId: question_id, topicName: topic, date: currentDate });
-            await markedQuestion.save();
-            return res.status(200).json({ data: "Marked", error: false });
-        }
-        // UNMARK QUESTION
-        await UserQuestions.deleteOne(userQuestionExists);
-        return res.status(200).json({ data: "Question unmarked successfully", error: true });
 
-    } catch (err) {
-        console.log(err);
-        return res.status(500).json({ data: "Something went wrong", error: true });
-    }
-});
+// Mark Question
+router.post("/markQuestion", markQuestion);
 
 
 module.exports = router;
